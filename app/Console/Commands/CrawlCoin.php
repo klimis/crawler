59c49b890cf0b14b44791dc3ab04242bb00e0d7b
Klimis <klimiss@gmail.com>
2025-07-15 19:58:57 +0300

    added request ip

--- app/Console/Commands/CrawlCoin.php
+++ app/Console/Commands/CrawlCoin.php
@@ -6,6 +6,7 @@ namespace App\Console\Commands;
 use App\Observers\CrawlerObserver;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Process;
 use Illuminate\Support\Facades\Storage;
 use Spatie\Browsershot\Browsershot;
 use Spatie\Crawler\Crawler;
@@ -17,7 +18,7 @@ class CrawlCoin extends Command
      *
      * @var string
      */
-    protected $signature = 'app:crawl-coin';
+    protected $signature = 'crawl-coin';
 
     /**
      * The console command description.
@@ -26,37 +27,58 @@ class CrawlCoin extends Command
      */
     protected $description = 'Command description';
 
+    protected $env = 'production'; // or 'production' 'staging', 'development', etc.
+
+    protected $vpnCountries = [
+        'Italy',
+        'Germany',
+        'France',
+        'Spain',
+        'Netherlands',
+        'Poland',
+        'Sweden',
+        'Finland',
+        'Belgium',
+        'Austria',
+        'Greece',
+        'Portugal',
+        'Norway',
+        'Denmark',
+        'Ireland',
+
+    ];
+
+    public function ip()
+    {
+
+        //get random country from $vpnCountries
+        $result = Process::run('nordvpn connect ' . $this->vpnCountries[array_rand($this->vpnCountries)]);
+        return ($result->output());
+    }
+
     /**
      * Execute the console command.
      */
     public function handle()
     {
-        $pages = static::pages();
-        //choose random 100 pages
-        $pages = array_slice($pages, 0, 100);
-        //random sort pages
-        shuffle($pages);
+        for ($i = 0; $i < 5; $i++) {
+            $vpn = $this->ip();
+            Log::info($vpn);
 
+            $pages = static::pages();
+            shuffle($pages);
+            $pages = array_slice($pages, 0, rand(5, 15));
 
-//        $pages = [
-//            'https://demo.coin-dev.eu/explorer/indices/ihdi/inequality–adjusted-human-development-index',
-//            'https://demo.coin-dev.eu/explorer/indices/ceii/clean-energy-innovation-index'];
-        static::get($pages);
+            static::appendPages($pages);
+            static::env($pages, $this->env);
+            static::get($pages);
+        }
+        Log::debug(static::disconnect());
     }
 
-    public static function get(array $pages = [])
-    {
-
-        foreach ($pages as $page) {
-            Log::debug('Browsershot page: ' . $page);
-            $html = static::shot($page);
-
-            if ($html) {
-                Log::debug('Save page: ' . $page);
-                $filename = 'crawled/' . static::pageName($page) . '.html';
-                Storage::disk('local')->put($filename, $html);
-            }
-        }
+    public static function disconnect(){
+        $result = Process::run('nordvpn disconnect');
+        return ($result->output());
     }
 
     public static function shot(string $url)
@@ -71,10 +93,22 @@ class CrawlCoin extends Command
                 ->evaluate('document.documentElement.outerHTML'); // Get complete rendered HTML
         } catch (\Exception $e) {
             Log::error('Browsershot error for URL: ' . $url . ' - ' . $e->getMessage());
-            //return '';
         }
         return $parse;
 
+    }
+
+    public static function get(array $pages = [])
+    {
+        foreach ($pages as $page) {
+            Log::debug('Browsershot page: ' . $page);
+            $html = static::shot($page);
+//            if ($html) {
+//                Log::debug('Save page: ' . $page);
+//                $filename = 'crawled/' . static::pageName($page) . '.html';
+//                Storage::disk('local')->put($filename, $html);
+//            }
+        }
     }
 
     public static function pageName(string $url)
@@ -88,7 +122,7 @@ class CrawlCoin extends Command
         return $lastPart;
     }
 
-    public static function pages()
+    public static function pages(string $env = 'production')
     {
         //get pages from here https://composite-indicators.jrc.ec.europa.eu/api/v1/en/sitemap.xml
         $sitemapUrl = 'https://composite-indicators.jrc.ec.europa.eu/api/v1/en/sitemap.xml';
@@ -102,13 +136,35 @@ class CrawlCoin extends Command
         $pages = [];
         foreach ($xml->url as $url) {
             $loc = (string)$url->loc;
-            if (strpos($loc, 'explorer/indices/') !== false || strpos($loc, 'explorer/scoreboards/') !== false) {
+
+            if (strpos($loc, 'explorer/indices/') !== false
+                || strpos($loc, 'explorer/scoreboards/') !== false
+                || strpos($loc, 'explorer/countries-and-territories/') !== false
+            ) {
                 $pages[] = $loc;
             }
         }
-
         return $pages;
 
+    }
+
+    public static function env(&$pages = [], $env)
+    {
+        if ($env !== 'production') {
+            //replace for each page the domain to demo.coin-dev.eu
+            $pages = array_map(function ($page) {
+                return str_replace('composite-indicators.jrc.ec.europa.eu', 'demo.coin-dev.eu', $page);
+            }, $pages);
+        }
+    }
+
+    public static function appendPages(&$pages = [])
+    {
+        return $pages = array_merge([
+            'https://composite-indicators.jrc.ec.europa.eu/explorer',
+            'https://composite-indicators.jrc.ec.europa.eu/explorer/indices-and-scoreboards',
+            'https://composite-indicators.jrc.ec.europa.eu/explorer/countries-and-territories',
+        ], $pages);
     }
 
     public static function crawl()
